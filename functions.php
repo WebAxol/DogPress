c51b635116d5dd6e7af549a88feb5afe7f950663
WebAxol <webaxol@gmail.com>
2023-12-25 08:23:50 -0600

    We worked on style using bootstrap. The top navigation menu was added bootstrap classes to make it pretty and responsive. That was done after creating the required wordpress hooks to add necessary classes to the generated elements. Furthermore, we added some style to the header section, using a downloaded banner image and a semi-opaque linear gradient above.

--- functions.php
+++ functions.php
@@ -66,8 +66,7 @@ function dw_register_styles(){
     
     $version = wp_get_theme()->get('Version');
     wp_enqueue_style('dw_bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css", array(), '5.0.1', 'all');
-    //wp_enqueue_style('dw_style', get_template_directory_uri() . "/style.css", array('dw_bootstrap'), $version, 'all');
-
+    wp_enqueue_style('dw_style', get_template_directory_uri() . "/style.css", array('dw_bootstrap'), $version, 'all');
 }
 
 add_action('wp_enqueue_scripts', 'dw_register_styles');
